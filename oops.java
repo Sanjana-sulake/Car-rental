9e1fbc3d1dce6431cfce9ba694f2e4d2fd1289c3
Sanjana-sulake <sanjanasulake10@gmail.com>
2024-12-20 20:30:31 +0530

    Add files via upload
    The car rental system that allows user to browse and book cars online,
    with PHP handling the web application and MySql storing information
    about cars, customers and bookings. The system provides an
    administrative interface for staff to manage inventory, bookings and
    customer info.

---
+++ oops.java
@@ -0,0 +1,15 @@
+class Student{
+    string name;
+    int age;
+    public void info(){
+        system.out.println(this.name);
+        system.out.println(this.age);
+    }
+}
+public class OOPS{
+    public static void main(string args[]){
+        stud s1=new student();
+        s1.name=sanju;
+        s1.age=20;
+    }
+}
